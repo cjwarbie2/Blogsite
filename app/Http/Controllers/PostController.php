7d32ac5e24208369b03cdd0f2df18f66f265bdf3
cjwarbie2 <59027886+cjwarbie2@users.noreply.github.com>
2019-12-18 18:11:04 +0000

    Add files via upload

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+Use App\Providers\Post;
+
+
+class  PostController extends Controller
+{
+
+
+  public function index()
+  {
+    $posts = Post::all();
+    return view('home', ['posts' => $posts]);
+    }
+}
